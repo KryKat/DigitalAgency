771579efec67c5dc5999d594b55a962f3097a6b3
KryKat <kate.krishtahovich@gmail.com>
2023-08-02 10:24:56 +0300

    add copyright

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ function navToggle() {
     document.getElementById("headerMenu").classList.toggle("_active");
     document.getElementById("burger").classList.toggle("_active");
 }
+document.getElementById("year").innerHTML = new Date().getFullYear();
